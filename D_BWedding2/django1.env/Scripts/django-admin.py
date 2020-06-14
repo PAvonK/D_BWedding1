3cefb60d3028b2104d77579de85d904a4e476e13
Fidranth <pavk13@outlook.com>
2020-06-13 20:15:06 -0600

    Delete Demo Web Site 2

--- D_BWedding2/django1.env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\programming_master\projects\d_bwedding2\django1.env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
